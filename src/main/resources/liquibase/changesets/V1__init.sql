ecc132cbf68f4a5c2a557420ed575d983d250e24
Farhod <wedsdsa22@gmail.com>
2024-04-30 11:03:26 -0700

    fIxed

--- src/main/resources/liquibase/changesets/V1__init.sql
+++ src/main/resources/liquibase/changesets/V1__init.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS product
     category_id      BIGINT        NULL,
     preview_image_id BIGINT        NULL,
     highlights       VARCHAR(1500) NULL,
-    CONSTRAINT fk_product_category_id FOREIGN KEY (category_id) REFERENCES category (id)
+    CONSTRAINT fk_product_category_id FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE ON UPDATE NO ACTION
 );
 
 CREATE TABLE IF NOT EXISTS images
